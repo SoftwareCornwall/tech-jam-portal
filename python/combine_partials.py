0b816e42839be41db73f4fdc616493a3ff48a5a7
WaterRaccoon <dylanshirley111@gmail.com>
2023-10-03 16:03:38 +0100

    Put repetitive code on combine_partials into a function

--- python/combine_partials.py
+++ python/combine_partials.py
@@ -3,28 +3,33 @@ current_folder = os.getcwd()
 parent_folder = os.path.dirname(current_folder)
 print("the folder is here" , current_folder)
 
+
+def combine(html_files):
+    # Initialize an empty string to store the combined HTML content
+    combined_html = ""
+
+    # Iterate through each HTML file and append its content to the combined_html string
+    for html_file in html_files:
+        html_file = str(current_folder+html_file)
+        with open(html_file, "r") as file:
+            combined_html += file.read()
+
+# Write the combined HTML content to the output file
+    with open(output_file, "w") as output:
+        output.write(combined_html)
+
+    print(f"Combined HTML written to {output_file}")
+
 # List of HTML file names to be combined
+
+
 html_files = ["/partials/header.html", "/content/home.html", "/partials/footer.html"]
 
 # Output file name for the combined HTML
 output_file = "index.html"
 
-# Initialize an empty string to store the combined HTML content
-combined_html = ""
+combine(html_files)
 
-# Iterate through each HTML file and append its content to the combined_html string
-for html_file in html_files:
-    html_file = str(current_folder+html_file)
-    with open(html_file, "r") as file:
-        combined_html += file.read()
-
-# Write the combined HTML content to the output file
-with open(output_file, "w") as output:
-    output.write(combined_html)
-
-print(f"Combined HTML written to {output_file}")
-
-# List of HTML file names to be combined
 
 
 # List of HTML file names to be combined
@@ -33,19 +38,4 @@ html_files = ["/partials/header.html", "/content/list.html", "/partials/footer.h
 # Output file name for the combined HTML
 output_file = "python.html"
 
-# Initialize an empty string to store the combined HTML content
-combined_html = ""
-
-# Iterate through each HTML file and append its content to the combined_html string
-for html_file in html_files:
-    html_file = str(current_folder+html_file)
-    with open(html_file, "r") as file:
-        combined_html += file.read()
-
-# Write the combined HTML content to the output file
-with open(output_file, "w") as output:
-    output.write(combined_html)
-
-print(f"Combined HTML written to {output_file}")
-
-# List of HTML file names to be combined
+combine(html_files)
